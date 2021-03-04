26882e246e759a30142cd6aca907f48cd90a2158
Samuel Tenschert <samuel.tenschert@gmail.com>
2021-03-04 15:04:57 +0100

    int 1

--- Day 2 - Basic/index.js
+++ Day 2 - Basic/index.js
@@ -1,8 +1,8 @@
-function crystalGazer (numbchild, partner, loc, job ) {
-    return (`You will be a ${job} in ${loc} and married to ${partner} with ${numbchild}`);    
-}
+// function crystalGazer (numbchild, partner, loc, job ) {
+//     return (`You will be a ${job} in ${loc} and married to ${partner} with ${numbchild}`);    
+// }
 
-console.log(crystalGazer(4, "Juan", "Vienna", "Webdeveloper"));
+// console.log(crystalGazer(4, "Juan", "Vienna", "Webdeveloper"));
 
 
 // // -----------------------------------------------------------------------------------------------------
@@ -16,11 +16,43 @@ console.log(crystalGazer(4, "Juan", "Vienna", "Webdeveloper"));
 
 // //-------------------------------------------------------------------------------------
 
-function ageCalculator(Birth) {
-    let d = new Date();
-    let age = d.getFullYear() - Birth;
-    return (`You are either ${age - 1} or ${age}`);
+// function ageCalculator(Birth) {
+//     let d = new Date();
+//     let age = d.getFullYear() - Birth;
+//     return (`You are either ${age - 1} or ${age}`);
     
-}
+// }
 
-document.write(ageCalculator(1990));
+// document.write(ageCalculator(1990));
+
+
+// ---------------------------------------------------------------------------------
+
+// let fahr = (celsius) => {
+//     let a = Math.PI / 180;
+//     let rad = celsius * a;
+//     return rad;
+// }
+
+// document.write(fahr(90));
+
+// --------------------------------------------------------------------------------------
+
+// let calc = (width, height, depth) => {
+//     let area = width * height;
+//     let vol = width * height * depth;
+//     return (`The area of the box is: ${area}`+"<br>"+`The volume of the box is: ${vol}`);
+// }
+// document.write(calc(7,2,5));
+
+// -----------------------------------------------------------------------------------------
+
+// let capitalize = (function() {
+//     let name = "i am a webdeveloper";
+//     let res = name.charAt(0).toUpperCase() + name.slice(1);
+//     let textp = document.write(res);
+//     return (textp);
+// }());
+
+// --------------------------------------------------------------------------------
+
